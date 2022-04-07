79991be63e9f4ec946248ac9b2e99bd119b9655d
JoonKim <justinkim36@naver.com>
2022-04-08 07:43:00 +0900

    Initial commit

---
+++ src/model/Room.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Room implements IROOM{
+    @Override
+    public String getRoomNumber() {
+        return null;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return null;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return null;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+}
